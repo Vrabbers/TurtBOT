bce94b8c99deb3e424b734936b9bced9c3a54a26
Vrabbers <nininininimocp@gmail.com>
2019-09-01 14:41:05 -0300

    config

--- TurtBOT/Program.cs
+++ TurtBOT/Program.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace TurtBOT
 {
     class Program
     {
-        
+           
         public static async Task Main(string[] args)
         {
             string token;
+            BotConfig config;
             if (!File.Exists("token.txt"))
             {
                 Console.Write("Please insert your bot account's token:");
@@ -18,10 +20,26 @@ namespace TurtBOT
                 file.Write(token);
             }
             token = File.ReadAllText("token.txt");
-            var config = new BotConfig(){Prefix = "tb:"};
+
+            if (!File.Exists("config.json"))
+            {
+                config = Config();
+                var configJson = JsonConvert.SerializeObject(config);
+                await using var file = File.CreateText("config.json");
+                file.Write(configJson);
+            }
+
+            config = JsonConvert.DeserializeObject<BotConfig>(File.ReadAllText("config.json"));
             Bot.Initialize(token,config);
             Console.ReadLine();
         }
 
+        static BotConfig Config(BotConfig bc = new BotConfig())
+        {
+            Console.Write("Prefix [{0}]:", bc.Prefix);
+            var pref = Console.ReadLine();
+            if (pref != "") bc.Prefix = pref;
+            return bc;
+        }
     }
 }
